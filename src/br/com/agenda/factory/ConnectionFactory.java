8a69f5b2a43036a5ce7ab7822dc88ec0348ec040
GustavoBryan <devgustavob@gmail.com>
2023-03-25 12:41:38 -0300

    Moantando DAO

---
+++ src/br/com/agenda/factory/ConnectionFactory.java
@@ -0,0 +1,13 @@
+package br.com.agenda.factory;
+
+public class ConnectionFactory {
+	
+	//nome do usúario mysql
+	private static final String USERNAME = "root";
+	
+	//senha do banco
+	private static final String PASSWORD = "";
+	
+	//
+
+}
